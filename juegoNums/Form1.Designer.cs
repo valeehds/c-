7d701e1c6707b4cfa84d980bd32a7246cd15c150
valeehds <thetrukis@gmail.com>
2024-04-05 00:17:01 -0500

    Nuevos archivos

---
+++ juegoNums/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace juegoNums
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numero = new System.Windows.Forms.TextBox();
+            this.BtnComprobar = new System.Windows.Forms.Button();
+            this.intentos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.panelNumeros = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numero
+            // 
+            this.numero.BackColor = System.Drawing.Color.White;
+            this.numero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numero.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero.ForeColor = System.Drawing.Color.LimeGreen;
+            this.numero.Location = new System.Drawing.Point(46, 172);
+            this.numero.Multiline = true;
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(329, 31);
+            this.numero.TabIndex = 0;
+            this.numero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnComprobar
+            // 
+            this.BtnComprobar.BackColor = System.Drawing.Color.LightGreen;
+            this.BtnComprobar.CausesValidation = false;
+            this.BtnComprobar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnComprobar.FlatAppearance.BorderSize = 0;
+            this.BtnComprobar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnComprobar.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnComprobar.Location = new System.Drawing.Point(381, 172);
+            this.BtnComprobar.Name = "BtnComprobar";
+            this.BtnComprobar.Size = new System.Drawing.Size(136, 31);
+            this.BtnComprobar.TabIndex = 1;
+            this.BtnComprobar.Text = "Comprobar";
+            this.BtnComprobar.UseVisualStyleBackColor = false;
+            this.BtnComprobar.Click += new System.EventHandler(this.BtnComprobar_Click);
+            // 
+            // intentos
+            // 
+            this.intentos.BackColor = System.Drawing.Color.Honeydew;
+            this.intentos.Cursor = System.Windows.Forms.Cursors.No;
+            this.intentos.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intentos.ForeColor = System.Drawing.Color.LimeGreen;
+            this.intentos.Location = new System.Drawing.Point(451, 50);
+            this.intentos.Name = "intentos";
+            this.intentos.Size = new System.Drawing.Size(66, 65);
+            this.intentos.TabIndex = 2;
+            this.intentos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(305, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Intento número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingresa un número";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Números ingresados";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.Font = new System.Drawing.Font("Century Gothic", 8F);
+            this.labelResultado.Location = new System.Drawing.Point(42, 235);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(475, 26);
+            this.labelResultado.TabIndex = 7;
+            this.labelResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelNumeros
+            // 
+            this.panelNumeros.BackColor = System.Drawing.Color.Honeydew;
+            this.panelNumeros.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.panelNumeros.Location = new System.Drawing.Point(46, 304);
+            this.panelNumeros.Name = "panelNumeros";
+            this.panelNumeros.Size = new System.Drawing.Size(471, 56);
+            this.panelNumeros.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(151, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnComprobar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(557, 408);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelNumeros);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.intentos);
+            this.Controls.Add(this.BtnComprobar);
+            this.Controls.Add(this.numero);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Adivinar números";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numero;
+        private System.Windows.Forms.Button BtnComprobar;
+        private System.Windows.Forms.Label intentos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Panel panelNumeros;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ juegoNums/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace juegoNums
+{
+    public partial class Form1 : Form
+    {
+        int numeroRandom;
+        int intentosRestantes;
+        List<string> numerosIngresados; // Lista para almacenar los números ingresados
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReiniciarJuego();
+            panelNumeros.BackColor = Color.FromArgb(224, 242, 229 );
+            intentos.BackColor = Color.FromArgb(224, 242, 229);
+            //BtnComprobar.BackColor = Color.FromArgb(117, 233, 153);
+            CenterToScreen();
+        }
+
+        private void BtnComprobar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(numero.Text, out int intento) && intento >= 1 && intento <= 100) // Intentar convertir el texto a un número y verificar que esté en el rango
+            {
+                if (numerosIngresados.Contains(intento.ToString())) // Verificar si el número ya ha sido ingresado
+                {
+                    labelResultado.ForeColor = Color.Red;
+                    labelResultado.Text = "Este número ya ha sido ingresado. Intenta con otro.";
+                    numero.Text = "";
+                    return;
+                }
+
+                numerosIngresados.Add(intento.ToString()); // Agregar el número ingresado a la lista
+                intentosRestantes--; // Reducir el número de intentos restantes
+
+                if (intento == numeroRandom) // Si el número es correcto
+                {
+                    labelResultado.ForeColor = Color.Green;
+                    labelResultado.Text = "Correcto, el número sí era " + $"{numeroRandom}";
+                    MostrarMensajeNuevoJuego();
+                }
+                else
+                {
+                    if (intento < numeroRandom)
+                    {
+                        labelResultado.ForeColor = Color.DarkGreen;
+                        labelResultado.Text = "El número es mayor. Intenta de nuevo.";
+                    }
+                    else
+                    {
+                        labelResultado.ForeColor = Color.Green;
+                        labelResultado.Text = "El número es menor. Intenta de nuevo.";
+                    }
+
+                    if (intentosRestantes == 0) // Si se quedan sin intentos
+                    {
+                        labelResultado.ForeColor = Color.Blue;
+                        labelResultado.Text = $"Se acabaron los intentos. El número era: {numeroRandom}";
+                        MostrarMensajeNuevoJuego();
+                    }
+                }
+
+                intentos.Text = intentosRestantes.ToString();
+                MostrarNumerosIngresados();
+                numero.Text = ""; // Limpiar el TextBox de entrada para el próximo número
+            }
+            else
+            {
+                labelResultado.ForeColor = Color.Red;
+                if (intento < 1 || intento > 100)
+                {
+                    labelResultado.Text = "Por favor, introduce un número válido entre 1 y 100.";
+                    numero.Text = "";
+                }
+            }
+        }
+
+        private void ReiniciarJuego()
+        {
+            numeroRandom = new Random().Next(1, 100);
+            intentosRestantes = 10;
+            intentos.Text = $"{intentosRestantes}";
+            numerosIngresados = new List<string>();
+            labelResultado.Text = "";
+        }
+
+        private void MostrarMensajeNuevoJuego()
+        {
+            DialogResult resultado = MessageBox.Show("¿Quieres jugar de nuevo?", "Nuevo juego", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (resultado == DialogResult.Yes)
+            {
+                ReiniciarJuego();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void MostrarNumerosIngresados()
+        {
+            // Limpiar el panel antes de mostrar los números
+            panelNumeros.Controls.Clear();
+
+            int panelWidth = panelNumeros.Width;
+            int totalLabelWidth = 0;
+
+            foreach (string num in numerosIngresados)
+            {
+                Label labelNumero = new Label();
+                labelNumero.Text = num;
+                labelNumero.AutoSize = true;
+                labelNumero.Font = new Font("Century Gothic", 10); // Establecer la fuente
+                if (num == numeroRandom.ToString())
+                {
+                    labelNumero.ForeColor = Color.Red; // Si el número ingresado es correcto, colorearlo de verde
+                }
+                else
+                {
+                    labelNumero.ForeColor = Color.Green; // Si el número ingresado no es correcto, colorearlo de rojo
+                }
+                panelNumeros.Controls.Add(labelNumero); // Agregar el label al panel
+
+                totalLabelWidth += labelNumero.Width;
+            }
+
+            int offsetX = (panelWidth - totalLabelWidth) / 2;
+            int posX = offsetX;
+
+            foreach (Control control in panelNumeros.Controls)
+            {
+                control.Location = new Point(posX, 10); // Posición del label
+                posX += control.Width; // Incrementar la posición X para el próximo label
+            }
+        }
+
+    }
+}
